06d11534cb6b3fc10406ab36c17f75b1c7aeb6de
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-06-24 10:41:02 +0100

    commit

--- kernel/run.sh
+++ kernel/run.sh
@@ -5,16 +5,16 @@ as boot.s -o boot.o
 gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
 #linking the kernel with kernel.o and boot.o files
-gcc -T linker.ld -o MyOS.bin -ffreestanding -O2 -nostdlib kernel.o boot.o -lgcc
+gcc -T linker.ld -o fgOS.bin -ffreestanding -O2 -nostdlib kernel.o boot.o -lgcc
 
 #check MyOS.bin file is x86 multiboot file or not
 grub-file --is-x86-multiboot MyOS.bin
 
 #building the iso file
 mkdir -p isodir/boot/grub
-cp MyOS.bin isodir/boot/MyOS.bin
+cp fgOS.bin isodir/boot/fgOS.bin
 cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o MyOS.iso isodir
+grub-mkrescue -o fgOS.iso isodir
 
 #run it in qemu
-qemu-system-x86_64 -cdrom MyOS.iso
+qemu-system-x86_64 -cdrom fgOS.iso
