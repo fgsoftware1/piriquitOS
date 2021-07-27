9d911306b12ba4ba682be02b596af66241fb4450
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-27 12:02:05 +0100

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,16 +1,20 @@
-void printf(char* str){
-	unsigned short* videoMemory = (unsigned short*)0xb8000;
+void printf (char* str) {
+    static unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    }
+}
 
-	for(int i = 0; str[i] != '\0'; ++i)
-		videoMemory[i] = (videoMemory[i] & 0xFF0) | str[i];
+extern "C" void fgosMain () {
+    printf("Hello World");
+    while(1);
 }
 
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
-
-extern "C" void KernelMain(void* multiboot_structure, unsigned int){
-    printf("hell world!");
-
-	while (1);
+extern "C" void callContructors () {
+    for(constructor* i = &start_ctors; i != &end_ctors; i++) {
+        (*i)();
+    }
 }
