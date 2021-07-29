77b5b17d46f8bbc84b9893c08a1b787d5104b33f
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-29 19:30:58 +0100

    Update gdt.cpp

--- src/gdt.cpp
+++ src/gdt.cpp
@@ -25,7 +25,7 @@ uint16_t GlobalDescriptorTable::CodeSegmentSelector(){
   return (uint8_t*)&codeSegmentSelector - (uint8_t*)this;
 }
 
-GlobalDescriptorTable::SegmentDescriptor::SegmentSelector(uint32_t base, uint32_t limit,uint8_t flags){
+GlobalDescriptorTable::SegmentDescriptor::SegmentDescriptor(uint32_t base, uint32_t limit,uint8_t flags){
   uint8_t* target = (uint8_t*)this;
 
   if(limit < 65556){
