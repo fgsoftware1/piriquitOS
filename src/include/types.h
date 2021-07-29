914dfce3f6636e86fda108f6a0fbbada532e0818
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-29 11:33:39 +0100

    Update types.h

--- src/include/types.h
+++ src/include/types.h
@@ -7,7 +7,7 @@ typedef short int16_t;
 typedef unsigned short uint16_t;
 
 typedef int int32_t;
-typedef unsigned uint32_t
+typedef unsigned uint32_t;
 
 typedef long long int int64_t;
 typedef unsigned long long int uint64_t;
