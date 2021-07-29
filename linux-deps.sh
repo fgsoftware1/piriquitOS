c2e7862a58f347190b76b109322757ccd0b39774
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-29 19:20:44 +0100

    Update linux-deps.sh

--- linux-deps.sh
+++ linux-deps.sh
@@ -2,4 +2,4 @@
 
 apt-get update -y && apt-get upgrade -y
 apt-get install -y apt-utils
-apt-get install -y git gcc ninja-build grub-common build-essential
+apt-get install -y git gcc ninja-build grub-common build-essential gcc-multilib
