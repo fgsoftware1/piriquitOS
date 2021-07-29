73a711dadf3e54626c0809792b4b9615dd4b9594
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-29 13:03:20 +0100

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -8,6 +8,15 @@ void printf (char* str) {
   }
 }
 
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+  for(constructor* i = &start_ctors; i != &end_ctors; i++)
+    (*i)();
+}
+
 extern "C" void fgosMain (const void* multiboot_structure, uint32_t) {
   printf("Hello World");
   GlobalDescriptorTable gdt;
