065ee6cb188eaeaf272f7e9823bd701d3e79f56f
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-29 20:27:28 +0100

    Fgsoftware1/issue39 (#42)
    * Update gdt.h
    
    * Create cstdint.h
    
    * Update gdt.h
    
    * Update cstdint.h

--- src/libs/cstdint.h
+++ src/libs/cstdint.h
@@ -1,45 +1,13 @@
 namespace std {
-  using int8_t         = /* signed integer type */;   // optional
-  using int16_t        = /* signed integer type */;   // optional
-  using int32_t        = /* signed integer type */;   // optional
-  using int64_t        = /* signed integer type */;   // optional
-  using intN_t         = /* see description */;       // optional, see description
- 
-  using int_fast8_t    = /* signed integer type */;
-  using int_fast16_t   = /* signed integer type */;
-  using int_fast32_t   = /* signed integer type */;
-  using int_fast64_t   = /* signed integer type */;
-  using int_fastN_t    = /* see description */;       // optional, see description
- 
-  using int_least8_t   = /* signed integer type */;
-  using int_least16_t  = /* signed integer type */;
-  using int_least32_t  = /* signed integer type */;
-  using int_least64_t  = /* signed integer type */;
-  using int_leastN_t   = /* see description */;       // optional, see description
- 
-  using intmax_t       = /* signed integer type */;
-  using intptr_t       = /* signed integer type */;   // optional
- 
-  using uint8_t        = /* unsigned integer type */; // optional
-  using uint16_t       = /* unsigned integer type */; // optional
-  using uint32_t       = /* unsigned integer type */; // optional
-  using uint64_t       = /* unsigned integer type */; // optional
-  using uintN_t        = /* see description */;       // optional, see description
- 
-  using uint_fast8_t   = /* unsigned integer type */;
-  using uint_fast16_t  = /* unsigned integer type */;
-  using uint_fast32_t  = /* unsigned integer type */;
-  using uint_fast64_t  = /* unsigned integer type */;
-  using uint_fastN_t   = /* see description */;       // optional, see description
- 
-  using uint_least8_t  = /* unsigned integer type */;
-  using uint_least16_t = /* unsigned integer type */;
-  using uint_least32_t = /* unsigned integer type */;
-  using uint_least64_t = /* unsigned integer type */;
-  using uint_leastN_t  = /* see description */;       // optional, see description
- 
-  using uintmax_t      = /* unsigned integer type */;
-  using uintptr_t      = /* unsigned integer type */; // optional
+  using int8_t         = signed int8_t;
+  using int16_t        = signed int16_t;
+  using int32_t        = signed int32_t;
+  using int64_t        = signed int64_t
+  
+  using uint8_t        = unsigned uint8_t;
+  using uint16_t       = unsigned uint16_t;
+  using uint32_t       = unsigned uint32_t;
+  using uint64_t       = unsigned uint64_t;
 }
  
 #define INTN_MIN         /* see description */
