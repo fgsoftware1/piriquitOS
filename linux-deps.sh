01bd92c770a116e149d25fa164e9a2675e5f82ee
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-30 16:31:27 +0100

    Update linux-deps.sh

--- linux-deps.sh
+++ linux-deps.sh
@@ -3,7 +3,7 @@
 apt-get update -y && apt-get upgrade -y
 apt-get install -y apt-utils --no-install-recommends
 apt-get install -y git gcc ninja-build grub-common build-essential gcc-multilib --no-install-recommends
-apt-get install --reinstall ca-certificates -y~
+apt-get install --reinstall ca-certificates -y
 mkdir /usr/local/share/ca-certificates/cacert.org
 wget -P /usr/local/share/ca-certificates/cacert.org http://www.cacert.org/certs/root.crt http://www.cacert.org/certs/class3.crt
 update-ca-certificates
