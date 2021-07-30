8aab86bd3308dadd70a058582cde7394c414eb07
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-30 17:38:35 +0100

    Update linux-deps-original.sh

--- linux-deps-original.sh
+++ linux-deps-original.sh
@@ -2,7 +2,7 @@
 
 apt-get update -y && apt-get upgrade -y
 apt-get install -y apt-utils=2.0.6 --no-install-recommends
-apt-get install -y git=2.25.1 gcc=9.3.0 ninja-build1.2.0 grub-common:2.0.4 build-essential:12.8 gcc-multilib=9.3.0 --no-install-recommends
+apt-get install -y git=2.25.1-1ubuntu3.1 gcc=9.3.0-1ubuntu2 ninja-build1.2.0 grub-common:2.04-1ubuntu26.12 build-essential:12.8ubuntu1.1 gcc-multilib=9.3.0-1ubuntu2 --no-install-recommends
 apt-get install --reinstall ca-certificates:20.04.1 -y
 mkdir /usr/local/share/ca-certificates/cacert.org
 wget -P /usr/local/share/ca-certificates/cacert.org http://www.cacert.org/certs/root.crt http://www.cacert.org/certs/class3.crt
