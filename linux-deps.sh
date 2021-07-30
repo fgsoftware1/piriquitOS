6fb0483f78984ee8e43f3aa1cabf817673f3203f
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-30 13:47:37 +0100

    Update linux-deps.sh

--- linux-deps.sh
+++ linux-deps.sh
@@ -3,3 +3,4 @@
 apt-get update -y && apt-get upgrade -y
 apt-get install -y apt-utils
 apt-get install -y git gcc ninja-build grub-common build-essential gcc-multilib
+git clone --depth 1 https://github.com/fgsoftware1/fgOS-BETA.git
