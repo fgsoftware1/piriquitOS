a3641b2bafa279e1391e2b9361f1908f5c789ed4
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-30 16:30:17 +0100

    Update linux-deps.sh

--- linux-deps.sh
+++ linux-deps.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 apt-get update -y && apt-get upgrade -y
-apt-get install -y apt-utils
-apt-get install -y git gcc ninja-build grub-common build-essential gcc-multilib
+apt-get install -y apt-utils --no-install-recommends
+apt-get install -y git gcc ninja-build grub-common build-essential gcc-multilib --no-install-recommends
+apt-get install --reinstall ca-certificates -y~
+mkdir /usr/local/share/ca-certificates/cacert.org
+wget -P /usr/local/share/ca-certificates/cacert.org http://www.cacert.org/certs/root.crt http://www.cacert.org/certs/class3.crt
+update-ca-certificates
+git config --global http.sslCAinfo /etc/ssl/certs/ca-certificates.crt
 git clone --depth 1 https://github.com/fgsoftware1/fgOS-BETA.git
