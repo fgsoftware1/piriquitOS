6e15408f4c05e7155dc5169fcdde653435dcc61a
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-08-01 00:50:42 +0100

    Update gdt.cpp

--- src/gdt.cpp
+++ src/gdt.cpp
@@ -1,13 +1,13 @@
 #include "include/gdt.h"
 
-GlobalDescriptorTable::GlobalDescriptorTable()
-    : nullSegmentSelector(0, 0, 0),
-        unusedSegmentSelector(0, 0, 0),
-        codeSegmentSelector(0, 64*1024*1024, 0x9A),
-        dataSegmentSelector(0, 64*1024*1024, 0x92)
+uint32_t GlobalDescriptorTable::GlobalDescriptorTable()
+: nullSegmentSelector(0, 0, 0),
+unusedSegmentSelector(0, 0, 0),
+codeSegmentSelector(0, 64*1024*1024, 0x9A),
+dataSegmentSelector(0, 64*1024*1024, 0x92)
 {
     uint32_t i[2];
-    (uint32_t)i[1] = (uint32_t)this;
+    i[1] = (uint32_t)this;
     i[0] = sizeof(GlobalDescriptorTable) << 16;
     asm volatile("lgdt (%0)": :"p" (((uint8_t *) i)+2));
 }
