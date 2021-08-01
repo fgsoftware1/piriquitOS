060ab030273e04b99690620b1d467cbd79d94db4
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-08-01 19:07:31 +0100

    commit

--- src/boot.s
+++ src/boot.s
@@ -1,29 +1,37 @@
-.set ALIGN,    1<<0
-.set MEMINFO,  1<<1
-.set FLAGS,    ALIGN | MEMINFO
-.set MAGIC,    0x1BADB002
-.set CHECKSUM, -(MAGIC + FLAGS)
+BITS 16
 
-.section .multiboot
-.align 4
-.long MAGIC
-.long FLAGS
-.long CHECKSUM
+start:
+	mov ax, 07C0h		; Set up 4K stack space after this bootloader
+	add ax, 288		; (4096 + 512) / 16 bytes per paragraph
+	mov ss, ax
+	mov sp, 4096
 
-.section .bss
-.align 16
-stack_bottom:
-.skip 16384 # 16 KiB
-stack_top:
+	mov ax, 07C0h		; Set data segment to where we're loaded
+	mov ds, ax
 
-.section .text
-.global _start
-.type _start, @function
-_start:
-  mov $stack_top, %esp
-call kernel_main
-cli
-1:	hlt
-	jmp 1b
-  
-.size _start, . - _start
+
+	mov si, text_string	; Put string position into SI
+	call print_string	; Call our string-printing routine
+
+	jmp $			; Jump here - infinite loop!
+
+
+	text_string db 'This is my cool new OS!', 0
+
+
+print_string:			; Routine: output string in SI to screen
+	mov ah, 0Eh		; int 10h 'print char' function
+
+.repeat:
+	lodsb			; Get character from string
+	cmp al, 0
+	je .done		; If char is zero, end of string
+	int 10h			; Otherwise, print it
+	jmp .repeat
+
+.done:
+	ret
+
+
+	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
+	dw 0xAA55		; The standard PC boot signature
