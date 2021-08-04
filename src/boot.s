7ca377f4a1922d8acb095b7a2af6dec346c5a25e
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-08-04 17:46:23 +0100

    Update boot.s

--- src/boot.s
+++ src/boot.s
@@ -1,37 +1,29 @@
-BITS 16
+.set MAGIC, 0x1badb002
+.set FLAGS, (1<<0 | 1 <<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
 
-start:
-	mov ax, 07C0h		; Set up 4K stack space after this bootloader
-	add ax, 288		; (4096 + 512) / 16 bytes per paragraph
-	mov ss, ax
-	mov sp, 4096
+.section .multiboot
+	.long MAGIC
+	.long FLAGS
+	.long CHECKSUM
+	
+.section .text
+.extern fgosmain
+.extern callContructors
+.global loader
 
-	mov ax, 07C0h		; Set data segment to where we're loaded
-	mov ds, ax
+loader:
+	mov $kernel_stack, %esp
+	call callContructors
+	push %eax
+	push %ebx
+	call fgosmain
+	
+_stop:
+	cli
+	hlt
+	jmp _stop
 
-
-	mov si, text_string	; Put string position into SI
-	call print_string	; Call our string-printing routine
-
-	jmp $			; Jump here - infinite loop!
-
-
-	text_string db 'This is my cool new OS!', 0
-
-
-print_string:			; Routine: output string in SI to screen
-	mov ah, 0Eh		; int 10h 'print char' function
-
-.repeat:
-	lodsb			; Get character from string
-	cmp al, 0
-	je .done		; If char is zero, end of string
-	int 10h			; Otherwise, print it
-	jmp .repeat
-
-.done:
-	ret
-
-
-	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
-	dw 0xAA55		; The standard PC boot signature
+.section .bss
+.space 4*1024*1024
+kernel_stack:
