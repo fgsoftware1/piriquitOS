0bf8f9bea18be907a6383a2fff4088fe3d3cd73d
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-13 20:49:05 +0100

    Update linux-deps.sh

--- linux-deps.sh
+++ linux-deps.sh
@@ -2,8 +2,7 @@
 
 apt-get update -y && apt-get upgrade -y
 apt-get install -y apt-utils --no-install-recommends
-apt-get install -y git gcc ninja-build grub-common gcc-multilib ca-certificates --no-install-recommends
+apt-get install -y git gcc ninja-build grub-common gcc-multilib ca-certificates xorriso --no-install-recommends
 apt-get autoremove && apt-get clean
 mkdir /usr/local/share/ca-certificates/cacert.org
 git config --global http.sslCAinfo /etc/ssl/certs/ca-certificates.crt
-git clone --depth 1 https://github.com/fgsoftware1/fgOS.git
