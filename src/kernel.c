9e316a04aba659f5cb286f033afbc7f2c8b66cf4
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-13 18:30:05 +0100

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,101 +1,51 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include "include/kernel.h"
  
-enum vga_color {
-	VGA_COLOR_BLACK = 0,
-	VGA_COLOR_BLUE = 1,
-	VGA_COLOR_GREEN = 2,
-	VGA_COLOR_CYAN = 3,
-	VGA_COLOR_RED = 4,
-	VGA_COLOR_MAGENTA = 5,
-	VGA_COLOR_BROWN = 6,
-	VGA_COLOR_LIGHT_GREY = 7,
-	VGA_COLOR_DARK_GREY = 8,
-	VGA_COLOR_LIGHT_BLUE = 9,
-	VGA_COLOR_LIGHT_GREEN = 10,
-	VGA_COLOR_LIGHT_CYAN = 11,
-	VGA_COLOR_LIGHT_RED = 12,
-	VGA_COLOR_LIGHT_MAGENTA = 13,
-	VGA_COLOR_LIGHT_BROWN = 14,
-	VGA_COLOR_WHITE = 15,
-};
- 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
+uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color) 
 {
-	return fg | bg << 4;
-}
- 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
-{
-	return (uint16_t) uc | (uint16_t) color << 8;
-}
- 
-size_t strlen(const char* str) 
-{
-	size_t len = 0;
-	while (str[len])
-		len++;
-	return len;
-}
- 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
- 
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
- 
-void terminal_initialize(void) 
-{
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
-		}
-	}
-}
- 
-void terminal_setcolor(uint8_t color) 
-{
-	terminal_color = color;
-}
- 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
-{
-	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = vga_entry(c, color);
-}
- 
-void terminal_putchar(char c) 
-{
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
-	}
-}
- 
-void terminal_write(const char* data, size_t size) 
-{
-	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
-}
- 
-void terminal_writestring(const char* data) 
-{
-	terminal_write(data, strlen(data));
-}
- 
-void kernel_main(void) 
-{
-	terminal_initialize();
+  uint16 ax = 0;
+  uint8 ah = 0, al = 0;
 
-	terminal_writestring("Hello, kernel World!\n");
+  ah = back_color;
+  ah <<= 4;
+  ah |= fore_color;
+  ax = ah;
+  ax <<= 8;
+  al = ch;
+  ax |= al;
+
+  return ax;
+}
+ 
+void clear_vga_buffer(uint16 **buffer, uint8 fore_color, uint8 back_color)
+{
+  uint32 i;
+  for(i = 0; i < BUFSIZE; i++){
+    (*buffer)[i] = vga_entry(NULL, fore_color, back_color);
+  }
+}
+
+void init_vga(uint8 fore_color, uint8 back_color)
+{
+  vga_buffer = (uint16*)VGA_ADDRESS;  
+  clear_vga_buffer(&vga_buffer, fore_color, back_color); 
+}
+ 
+void kernel_main() 
+{
+	init_vga(WHITE, BLACK);
+
+  vga_buffer[0] = vga_entry('H', WHITE, BLACK);
+  vga_buffer[1] = vga_entry('e', WHITE, BLACK);
+  vga_buffer[2] = vga_entry('l', WHITE, BLACK);
+  vga_buffer[3] = vga_entry('l', WHITE, BLACK);
+  vga_buffer[4] = vga_entry('o', WHITE, BLACK);
+  vga_buffer[5] = vga_entry(' ', WHITE, BLACK);
+  vga_buffer[6] = vga_entry('W', WHITE, BLACK);
+  vga_buffer[7] = vga_entry('o', WHITE, BLACK);
+  vga_buffer[8] = vga_entry('r', WHITE, BLACK);
+  vga_buffer[9] = vga_entry('l', WHITE, BLACK);
+  vga_buffer[10] = vga_entry('d', WHITE, BLACK);
 }
