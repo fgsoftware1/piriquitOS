d44c342b8a481e75fb51fd9bd39767caa5200a98
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-15 14:04:25 +0100

    Update linux-deps.sh

--- linux-deps.sh
+++ linux-deps.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-apt-get update -y
+apt-get update
 apt-get install -y git gcc ninja-build gcc-multilib ca-certificates xorriso nasm grub-pc --no-install-recommends
 apt-get autoremove && apt-get clean
 mkdir /usr/local/share/ca-certificates/cacert.org
