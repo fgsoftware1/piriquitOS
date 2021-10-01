4b4869d0f58abd313711e43b85b8d659b3373080
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-01 22:45:30 +0100

    commit

--- linux-env.sh
+++ linux-env.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-docker pull fgygh/fgos-env:latest
 docker run --rm -it -v $pwd:/home/admin/Desktop/projects/fgOS fgygh/fgos-env:latest
--- win-env.cmd
+++ win-env.cmd
@@ -1,2 +1,1 @@
-docker pull fgygh/fgos-env:latest
 docker run --rm -it -v %cd%:/home/admin/Desktop/projects/fgOS fgygh/fgos-env:latest
