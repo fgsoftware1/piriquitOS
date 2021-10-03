6d160d6527c4d48db172653b58e97ac0128a58c3
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-03 14:13:54 +0100

    Update linux-deps.sh

--- linux-deps.sh
+++ linux-deps.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-apt-get update
 apt-get update && apt-get install -y git nasm gcc ninja-build gcc-multilib xorriso grub-pc make automake autoconf m4 make mtools --no-install-recommends
