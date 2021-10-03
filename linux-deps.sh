48d8ef639a0161a73f785d597a72be41e4f1b15d
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-03 21:32:21 +0100

    commit

--- linux-deps.sh
+++ linux-deps.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-
 apt-get update && apt-get install -y git nasm gcc ninja-build gcc-multilib xorriso grub-pc make automake autoconf m4 make mtools --no-install-recommends
--- linux-env.sh
+++ linux-env.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-
 docker run --rm -it -v $pwd:/home/admin/Desktop/projects/fgOS fgygh/fgos-env:master
