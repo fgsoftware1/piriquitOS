91b4aa6b0369a692c7de6e1fdbfc998f3a27ab3d
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-11-27 12:35:44 +0000

    update docker image

--- linux-env.sh
+++ linux-env.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run --rm -it -v $pwd:/home/admin/Desktop/projects/fgOS fgygh/fgos-env:master
+docker run --rm -it -v $pwd:/home/admin/Desktop/projects/fgOS fgygh/fgos-env:latest
