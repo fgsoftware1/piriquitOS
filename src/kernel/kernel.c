d287e5142367eee6ebcf079afa15268785d65af7
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-11-27 12:26:07 +0000

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,24 +1,19 @@
-#include<stdio.h>
 #include "../include/print.h"
 
 void kernel_main() {
   int number;
 
-  printf("Provide the number of asterisks by side: \n");
-  scanf("%d", &number);
-
   for (int i = 1; i <= number; i++) {
     for (int j = 1; j <= number; j++) {
       if (i == 1 || i == number || j == 1 || j == number){
-        printf("* ");
+        print_char("* ");
       }else{
-        printf("  ");
+        print_char("  ");
       }
     }
 
-    printf("\n");
+    print_char("\n");
   }
-  print_clear();
   print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
   print_str("Welcome to fgOS!");
 }
