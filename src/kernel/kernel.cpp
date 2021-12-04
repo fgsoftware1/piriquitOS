06fd12c43f015d5097a17bb5782ea7b5e2c36820
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-12-04 16:38:14 +0000

    Update kernel.cpp

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,7 +1,7 @@
 #include "../include/terminal.hpp"
 #include "../include/vga.hpp"
 
-void kernel_main() {
+extern "C" void kernel_main() {
   terminal_initialize();
   terminal_setcolor(COLOR_GREEN, COLOR_BLACK);
   terminal_writestring("wellcome to fgOS!");
