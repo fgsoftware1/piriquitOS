a64cdb87fd2cacd6f4e1d42f28d0ab53ef4e07b9
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-12-04 13:23:38 +0000

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,19 +1,6 @@
 #include "../include/print.h"
 
 void kernel_main() {
-  int number;
-
-  for (int i = 1; i <= number; i++) {
-    for (int j = 1; j <= number; j++) {
-      if (i == 1 || i == number || j == 1 || j == number){
-        print_str("*");
-      }else{
-        print_str("  ");
-      }
-    }
-
-    print_str("\n");
-  }
   print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
   print_str("Welcome to fgOS!");
 }
