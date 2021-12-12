c688e70cd10c55b50ad4eecdc9f998927c03fe7b
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-12-12 22:05:45 +0000

    commit

---
+++ src/kernel/src/include/vga.hpp
@@ -0,0 +1,33 @@
+#ifndef VGA_HPP
+#define VGA_HPP
+
+#include <stddef.h>
+#include <stdint.h>
+
+enum vga_color
+{
+	COLOR_BLACK = 0,
+	COLOR_BLUE = 1,
+	COLOR_GREEN = 2,
+	COLOR_CYAN = 3,
+	COLOR_RED = 4,
+	COLOR_MAGENTA = 5,
+	COLOR_BROWN = 6,
+	COLOR_LIGHT_GREY = 7,
+	COLOR_DARK_GREY = 8,
+	COLOR_LIGHT_BLUE = 9,
+	COLOR_LIGHT_GREEN = 10,
+	COLOR_LIGHT_CYAN = 11,
+	COLOR_LIGHT_RED = 12,
+	COLOR_LIGHT_MAGENTA = 13,
+	COLOR_LIGHT_BROWN = 14,
+	COLOR_WHITE = 15,
+};
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
+uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg);
+uint16_t vga_entry(char c, uint8_t color);
+
+#endif
---
+++ src/kernel/src/kernel.cpp
@@ -0,0 +1,9 @@
+#include "./include/terminal.hpp"
+#include "./include/vga.hpp"
+
+extern "C" void kernel_main() {
+  terminal_initialize();
+  terminal_setcolor(COLOR_GREEN, COLOR_BLACK);
+  terminal_writestring("wellcome to fgOS!");
+  for(;;);
+}
