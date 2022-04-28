41ee7b2dbb0ebe6b2f7d132d1e52ec9ed401d2b8
fgygh <fgygh@fboi.ga>
2022-04-28 21:16:47 +0100

    fix some issues
    Signed-off-by: fgygh <fgygh@fboi.ga>

--- src/kernel/acpi.cpp
+++ src/kernel/acpi.cpp
@@ -2,7 +2,7 @@
 #include "include/drivers/acpi.hpp"
 #include "include/utils/kernel.hpp"
 #include "include/timer.hpp"
-#include "include7paging.hpp"
+#include "include/paging.hpp"
 #include "include/console.hpp"
 #include "include/stl/types.hpp"
 
@@ -10,8 +10,8 @@ namespace {
 uint32_t SMI_CMD;
 uint8_t ACPI_ENABLE;
 uint8_t ACPI_DISABLE;
-uint32_t PM1a_CNT; 
-uint32_t PM1b_CNT; 
+uint32_t PM1a_CNT;
+uint32_t PM1b_CNT;
 uint16_t SLP_TYPa;
 uint16_t SLP_TYPb;
 uint16_t SLP_EN;
@@ -25,21 +25,21 @@ struct RSDPtr {
    uint8_t CheckSum;
    uint8_t OemID[6];
    uint8_t Revision;
-   uint32_t RsdtAddress; 
+   uint32_t RsdtAddress;
 };
 
 struct FACP {
    uint8_t Signature[4];
    uint32_t Length;
    uint8_t unneded1[40 - 8];
-   uint32_t DSDT; 
+   uint32_t DSDT;
    uint8_t unneded2[48 - 44];
-   uint32_t SMI_CMD; 
+   uint32_t SMI_CMD;
    uint8_t ACPI_ENABLE;
    uint8_t ACPI_DISABLE;
    uint8_t unneded3[64 - 54];
-   uint32_t PM1a_CNT_BLK; 
-   uint32_t PM1b_CNT_BLK; 
+   uint32_t PM1a_CNT_BLK;
+   uint32_t PM1b_CNT_BLK;
    uint8_t unneded4[89 - 72];
    uint8_t PM1_CNT_LEN;
 };
